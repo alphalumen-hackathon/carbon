1a5aca74681e9087b57fe66090147b232245fbc9
martinelipedro <martinelipedro@gmail.com>
2023-09-17 10:42:19 -0300

    feat: add erase button to LocationInput component

--- src/components/LocationInput/LocationInput.style.tsx
+++ src/components/LocationInput/LocationInput.style.tsx
@@ -5,6 +5,15 @@ export const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 2,
     width: "90%",
-    padding: 5,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 1,
+    paddingRight: 2,
+  },
+  input: {
+    flexGrow: 1,
+    height: 25,
+    marginRight: 10,
   },
 });
