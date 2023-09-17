c362627e5d2e7932f7f2a7c22e735b6626b68677
martinelipedro <martinelipedro@gmail.com>
2023-09-17 09:47:07 -0300

    feat: add navigation route to NavigationScreen

---
+++ src/screens/HomeScreen/HomeScreen.props.tsx
@@ -0,0 +1,5 @@
+import { NavigationProp } from "@react-navigation/native";
+
+export interface IHomeScreenProps {
+  navigation: NavigationProp<any, any>;
+}
--- src/screens/HomeScreen/HomeScreen.screen.tsx
+++ src/screens/HomeScreen/HomeScreen.screen.tsx
@@ -1,12 +1,20 @@
 import { View } from "react-native";
 
+import { IHomeScreenProps } from "./HomeScreen.props";
 import { styles } from "./HomeScreen.style";
 import RoundButton from "../../components/RoundButton/RoundButton.component";
 
-const HomeScreen = () => {
+const HomeScreen = (props: IHomeScreenProps) => {
+  const { navigation } = props;
+
   return (
     <View style={styles.container}>
-      <RoundButton text="Where to?" onPress={() => {}} />
+      <RoundButton
+        text="Where to?"
+        onPress={() => {
+          navigation.navigate("Navigation");
+        }}
+      />
     </View>
   );
 };
