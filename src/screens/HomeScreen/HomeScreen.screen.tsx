1ca68f6054a32c1a40f6a28b73c999da84246a92
martinelipedro <martinelipedro@gmail.com>
2023-09-16 22:20:57 -0300

    feat: add HomeScreen screen component
    Added files for HomeScreen screen component and stylesheet. App.tsx now using HomeScreen component.

---
+++ src/screens/HomeScreen/HomeScreen.screen.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { styles } from "./HomeScreen.style";
+
+const HomeScreen = () => {
+  return (
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+      </View>
+  );
+}
+
+export default HomeScreen;
