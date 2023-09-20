83f173d3d48b4ec4167a07aa98842d24f16cefd3
gdefaria <gabrielfaria.alonso@gmail.com>
2023-09-20 00:21:24 -0300

    feat: tracking user speed and incrementing credits

--- src/screens/CreditsScreen/CreditsScreen.screen.tsx
+++ src/screens/CreditsScreen/CreditsScreen.screen.tsx
@@ -1,14 +1,23 @@
 import { ScrollView, Text, View } from "react-native";
 
-import { styles } from "./CreditsScreen.style";
 import CarbonCreditActivity from "../../components/CarbonCreditActivity/CarbonCreditActivity.component";
+import CarbonActivities from "../../states/CarbonActivities.state";
+import { styles } from "./CreditsScreen.style";
 
 const CreditsScreen = () => {
+  const activities = CarbonActivities((state) => state.activities)
+
+  // Calculating total carbon credits by adding the credits of each activity
+  // Credits can't be negative, so it's locked to zero
+  const carbonCredits = Math.max(0, activities.reduce((accumulated, activity) => (
+    accumulated + activity.credits
+  ), 0))
+
   return (
     <View style={styles.container}>
       <View style={styles.creditsArea}>
         <Text style={styles.carbonCreditsLabel}>Carbon credits</Text>
-        <Text style={styles.carbonCreditsValue}>520</Text>
+        <Text style={styles.carbonCreditsValue}>{carbonCredits}</Text>
       </View>
 
       <View style={styles.activityArea}>
@@ -18,11 +27,7 @@ const CreditsScreen = () => {
           persistentScrollbar
           contentContainerStyle={styles.scrollableActivitiesArea}
         >
-          <CarbonCreditActivity
-            credits={50}
-            description="Activity"
-            date={new Date()}
-          />
+          {activities.map((activity, index) => <CarbonCreditActivity {...activity} key={index} />)}
         </ScrollView>
       </View>
     </View>
---
+++ src/states/CarbonActivities.state.ts
@@ -0,0 +1,26 @@
+import { create } from "zustand";
+
+interface Activity {
+	description: string
+	credits: number
+	date: Date
+}
+
+interface ICarbonActivities {
+	activities: Activity[]
+	create: (description: string, credits: number, date: Date) => void
+}
+
+const CarbonActivities = create<ICarbonActivities>((set) => ({
+	activities: [],
+
+	create: (description: string, credits: number, date: Date) => {
+		set((
+			state: { activities: Activity[] }) => (
+				{ activities: [ ...state.activities, { description, credits, date } ] }
+			)
+		)
+	}
+}))
+
+export default CarbonActivities
