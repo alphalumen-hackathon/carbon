96ce68bdc5c582ca7709e303d1fe6e6701cc15c9
martinelipedro <martinelipedro@gmail.com>
2023-10-09 14:44:38 -0300

    feat: add bottom tab navigator

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import "react-native-gesture-handler";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { DarkTheme, NavigationContainer } from "@react-navigation/native";
 import { useEffect } from "react";
 import { Platform } from "react-native";
 
 import { listenForSpeedChange } from "./src/controllers/SpeedController";
 import Credits from "./src/screens/Credits/Credits.screen";
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigation = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Credits" component={Credits} />
+  </Tab.Navigator>
+);
 
 const App = () => {
   useEffect(() => {
@@ -15,7 +24,7 @@ const App = () => {
 
   return (
     <NavigationContainer theme={DarkTheme}>
-      <Credits />
+      {TabNavigation()}
     </NavigationContainer>
   );
 };
