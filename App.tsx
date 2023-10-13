a02f7930e527ee68b8401477e9a6f05b4abc9015
martinelipedro <martinelipedro@gmail.com>
2023-10-13 17:49:15 -0300

    fix: remove header from stack navigator

--- App.tsx
+++ App.tsx
@@ -29,7 +29,7 @@ const App = () => {
 
   return (
     <NavigationContainer theme={DarkTheme}>
-      <Stack.Navigator>
+      <Stack.Navigator  screenOptions={{ headerShown: false }}>
         {isSigned ? (
           <Stack.Screen name="Tab" component={TabNavigation} />
         ) : (
