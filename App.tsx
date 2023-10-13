8193b625cb0cf90f09047d17c500b79a67dac8a1
martinelipedro <martinelipedro@gmail.com>
2023-10-12 22:03:27 -0300

    feat: integrate Shop on tab navigator

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { DarkTheme, NavigationContainer } from "@react-navigation/native";
 
 import Credits from "./src/screens/Credits/Credits.screen";
 import HomeScreen from "./src/screens/Home/Home.screen";
+import ShopScreen from "./src/screens/Shop/Shop.screen";
 import SocialScreen from "./src/screens/Social/Social.screen";
 
 const Tab = createBottomTabNavigator();
@@ -13,6 +14,7 @@ const TabNavigation = () => (
     <Tab.Screen name="Home" component={HomeScreen} />
     <Tab.Screen name="Credits" component={Credits} />
     <Tab.Screen name="Social" component={SocialScreen} />
+    <Tab.Screen name="Shop" component={ShopScreen} />
   </Tab.Navigator>
 );
 
