6baf90eaef5cbcd867c684f473d7a4f868eab99f
gdefaria <108313823+gdefaria@users.noreply.github.com>
2023-10-13 18:47:54 -0300

    feat: add custom navbar (#7)

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-import "react-native-gesture-handler";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { DarkTheme, NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 
+import "react-native-gesture-handler";
+import { createStackNavigator } from "@react-navigation/stack";
+import Navbar from "./src/components/Navbar/Navbar.component";
 import Credits from "./src/screens/Credits/Credits.screen";
 import HomeScreen from "./src/screens/Home/Home.screen";
+import ShopScreen from "./src/screens/Shop/Shop.screen";
+import SocialScreen from "./src/screens/Social/Social.screen";
 import LoginScreen from "./src/screens/Login/Login.screen";
 import RegisterScreen from "./src/screens/Register/Register.screen";
 import ShopScreen from "./src/screens/Shop/Shop.screen";
@@ -15,7 +18,10 @@ const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 const TabNavigation = () => (
-  <Tab.Navigator screenOptions={{ headerShown: false }}>
+  <Tab.Navigator
+    screenOptions={{ headerShown: false }}
+    tabBar={(props) => <Navbar {...props} />}
+  >
     <Tab.Screen name="Home" component={HomeScreen} />
     <Tab.Screen name="Credits" component={Credits} />
     <Tab.Screen name="Social" component={SocialScreen} />
---
+++ src/components/Navbar/Navbar.style.ts
@@ -0,0 +1,44 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    height: 50,
+    backgroundColor: "#0A0A0A",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  margin: {
+    width: "80%",
+    height: "70%",
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  unselected: {
+    backgroundColor: "#161616",
+    height: "100%",
+    aspectRatio: 1,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  selected: {
+    width: "32%",
+    height: "100%",
+    backgroundColor: "#091E14",
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    borderRadius: 8,
+  },
+  routeName: {
+    color: "white",
+    paddingRight: "20%",
+    fontWeight: "bold",
+  },
+  selectedSvg: {
+    marginLeft: "10%",
+  },
+});
+
+export default styles;
