18219a29618a3e7e013e49e981538c03fcd7c4f5
gdefaria <gabrielfaria.alonso@gmail.com>
2023-10-13 18:42:37 -0300

    feat: add custom navbar

--- App.tsx
+++ App.tsx
@@ -1,24 +1,24 @@
-import "react-native-gesture-handler";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { DarkTheme, NavigationContainer } from "@react-navigation/native";
+import "react-native-gesture-handler";
 
+import Navbar from "./src/components/Navbar/Navbar.component";
 import Credits from "./src/screens/Credits/Credits.screen";
 import HomeScreen from "./src/screens/Home/Home.screen";
-import LoginScreen from "./src/screens/Login/Login.screen";
 import ShopScreen from "./src/screens/Shop/Shop.screen";
 import SocialScreen from "./src/screens/Social/Social.screen";
-import RegisterScreen from "./src/screens/Register/Register.screen";
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigation = () => (
-  <Tab.Navigator screenOptions={{ headerShown: false }}>
+  <Tab.Navigator
+    screenOptions={{ headerShown: false }}
+    tabBar={(props) => <Navbar {...props} />}
+  >
     <Tab.Screen name="Home" component={HomeScreen} />
     <Tab.Screen name="Credits" component={Credits} />
     <Tab.Screen name="Social" component={SocialScreen} />
     <Tab.Screen name="Shop" component={ShopScreen} />
-    <Tab.Screen name="Login" component={LoginScreen} />
-    <Tab.Screen name="Register" component={RegisterScreen} />
   </Tab.Navigator>
 );
 
