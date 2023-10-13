ae3ac086321c97d7b3bea91a298b86e7db52a7bc
martinelipedro <martinelipedro@gmail.com>
2023-10-13 11:21:19 -0300

    fix: Address input modal height

--- src/components/AddressInputModal/AddressInputModal.style.ts
+++ src/components/AddressInputModal/AddressInputModal.style.ts
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   modalView: {
-    height: "100%",
+    flex: 1,
     display: "flex",
     flexDirection: "column",
     justifyContent: "space-around",
